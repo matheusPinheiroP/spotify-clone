61c2f0e33188ddf332d6d523aa753dd9f1c43afb
Mateus Pinheiro <mateuspinheiro@Mateuss-MacBook-Pro.local>
2020-02-05 19:38:13 -0300

    change songs pics

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,10 @@ import { Container, Content, CardImage, Direction } from "./styles";
 import Keys from "../../assets/tbk.jpeg";
 import Chico from "../../assets/cb.jpeg";
 import Lj from "../../assets/lj.jpeg";
+import ae from "../../assets/ae.jpeg";
+import nop from "../../assets/new.jpeg";
+import v from "../../assets/v.jpeg";
+import w from "../../assets/w.jpeg";
 
 export default function Home() {
   return (
@@ -41,24 +45,24 @@ export default function Home() {
         <h1>Não sai dos seus ouvidos</h1>
         <Direction>
           <CardImage>
-            <img src={Keys} alt="capa" />
-            <span>The Black Keys</span>
-            <p>Go</p>
+            <img src={nop} alt="capa" />
+            <span>New Noise</span>
+            <p>Already</p>
           </CardImage>
           <CardImage>
-            <img src={Chico} alt="capa" />
-            <span>Chico Buarque</span>
-            <p>O que será? A flor da Pele</p>
+            <img src={ae} alt="capa" />
+            <span>Caetano Veloso</span>
+            <p>Tigresa</p>
           </CardImage>
           <CardImage>
-            <img src={Lj} alt="capa" />
-            <span>Litte joy</span>
-            <p>Next time Around</p>
+            <img src={v} alt="capa" />
+            <span>Summer</span>
+            <p>Turn down</p>
           </CardImage>
           <CardImage>
-            <img src={Lj} alt="capa" />
-            <span>Litte joy</span>
-            <p>Next time Around</p>
+            <img src={w} alt="capa" />
+            <span>Jazz</span>
+            <p>Dream a little dream of me</p>
           </CardImage>
           <CardImage>
             <img src={Lj} alt="capa" />
